f1d47cb7a2b70873cabc6e886e45be785c72a489
Javesshhh <javeshlodha02@gmail.com>
2024-10-17 01:21:17 +0530

    commit1

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,4 @@
-const Booking = require('../models/Booking');
+const Booking = require('../models/booking');
 
 // Get all bookings based on centerId, sportId, and date
 exports.getBookings = async (req, res) => {
