530354ff365df432e63be0bfdd2cdf83d6e25be4
Javesshhh <javeshlodha02@gmail.com>
2024-10-17 01:17:48 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -14,10 +14,7 @@ app.use(bodyParser.json());
 // app.use(morgan('dev')); // Logging middleware
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sports-booking', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
